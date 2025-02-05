b6839281cfb7242fe6ce480453cc480c6ffbbd2e
Anthony Teye-Adjei <tonyteyeadjei@gmail.com>
2025-02-05 08:54:35 +0100

    initialize base project

---
+++ src/main/java/com/group1/shoprider/ShopriderApplication.java
@@ -0,0 +1,13 @@
+package com.group1.shoprider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopriderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopriderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group1/shoprider/ShopriderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group1.shoprider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopriderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
